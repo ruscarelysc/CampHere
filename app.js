81536ec60c8bec7221b46eb39754fe9a7b0c5eeb
Ruscarelys Canizalez <ruscarelys@gmail.com>
2022-05-05 14:58:45 +1000

    add start script

--- app.js
+++ app.js
@@ -159,6 +159,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err })
 });
 
-app.listen(3000, () => {
-  console.log("APP IS LITENING ON PORT 3000!")
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`APP IS LITENING ON PORT ${port}!`)
 });
