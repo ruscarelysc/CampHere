1532271096205a5682ccc312ba62e2957e640222
Ruscarelys Canizalez <ruscarelys@gmail.com>
2022-05-23 23:04:28 +1000

    Move server config after db conn

--- app.js
+++ app.js
@@ -30,131 +30,133 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
   console.log('Database connected');
+
+  const app = express();
+
+  app.engine('ejs', engine);
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'))
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(methodOverride('_method'))
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.use(mongoSanitize({
+    replaceWith: '_'
+  }))
+
+  const store = new MongoDbStore({
+    mongoUrl: dbUrl,
+    secret: 'secret!!!supersuper',
+    touchAfter: 24 * 60 * 60
+  });
+
+
+  store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+  })
+
+  const sessionConfig = {
+    store,
+    name: 'session',
+    secret: 'secret!!!supersuper',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+
+  }
+
+  app.use(session(sessionConfig));
+  app.use(flash());
+
+  const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+  ];
+
+  const styleSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css",
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+  ];
+  const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+  ];
+
+  const fontSrcUrls = [];
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+          "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+      },
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new LocalStrategy(User.authenticate()));
+
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+
+  app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+  })
+
+  app.use('/', userRoutes);
+  app.use('/campsites', campsiteRoutes)
+  app.use('/campsites/:id/reviews', reviewRoutes)
+
+
+  app.get('/', (req, res) => {
+    res.render('home')
+  });
+
+  app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+  });
+
+  app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+  });
+
+
   const port = process.env.PORT || 3000
   app.listen(port, () => {
     console.log(`APP IS LITENING ON PORT ${port}!`)
   });
-});
-
-const app = express();
-
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize({
-  replaceWith: '_'
-}))
-
-const store = new MongoDbStore({
-  mongoUrl: dbUrl,
-  secret: 'secret!!!supersuper',
-  touchAfter: 24 * 60 * 60
-});
-
-
-store.on("error", function (e) {
-  console.log("SESSION STORE ERROR", e)
-})
-
-const sessionConfig = {
-  store,
-  name: 'session',
-  secret: 'secret!!!supersuper',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  }
-
-}
-
-app.use(session(sessionConfig));
-app.use(flash());
-
-const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css",
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://api.mapbox.com/",
-  "https://kit.fontawesome.com/",
-  "https://cdnjs.cloudflare.com/",
-  "https://cdn.jsdelivr.net",
-];
-
-const styleSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css",
-  "https://kit-free.fontawesome.com/",
-  "https://api.mapbox.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://fonts.googleapis.com/",
-  "https://use.fontawesome.com/",
-  "https://cdn.jsdelivr.net",
-];
-const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
-  "https://events.mapbox.com/",
-];
-
-const fontSrcUrls = [];
-
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: [],
-      connectSrc: ["'self'", ...connectSrcUrls],
-      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-      workerSrc: ["'self'", "blob:"],
-      objectSrc: [],
-      imgSrc: [
-        "'self'",
-        "blob:",
-        "data:",
-        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
-        "https://images.unsplash.com/",
-      ],
-      fontSrc: ["'self'", ...fontSrcUrls],
-    },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  next();
-})
-
-app.use('/', userRoutes);
-app.use('/campsites', campsiteRoutes)
-app.use('/campsites/:id/reviews', reviewRoutes)
-
-
-app.get('/', (req, res) => {
-  res.render('home')
-});
-
-app.all ('*', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404))
-});
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = 'Something Went Wrong!'
-  res.status(statusCode).render('error', { err })
 });
